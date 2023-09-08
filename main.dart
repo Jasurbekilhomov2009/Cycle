f06a022fb9ed918f199da03456eb56e7a94a865f
Durdona <durdonajurakulova98@gmail.com>
2023-09-08 08:33:21 +0500

    new project

---
+++ main.dart
@@ -0,0 +1,38 @@
+
+
+
+import 'dart:io';
+
+void main(List<String> args) 
+{ // <----------1----------->
+  // final int quest = int.parse(stdin.readLineSync() ?? '0');
+
+
+  // for(var i = 0; i <= quest; i++) {
+  //   print('Element $i');
+  // }
+  // <----------1----------->
+   
+   // <----------2----------->
+  //   final int que = int.parse(stdin.readLineSync() ?? '0');
+
+  //  for(var i = 0; i == que; i--) {
+  //   print('Element $i');
+  // }
+  // <----------2----------->
+
+  // <----------3----------->
+   
+  final int inf = int.parse(stdin.readLineSync() ?? '0');
+
+  do {
+    print('Enter the number');
+
+    
+  }  while(inf != true);
+  // <----------3----------->
+  
+
+
+  
+}
