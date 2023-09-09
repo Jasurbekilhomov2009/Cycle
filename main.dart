b18adef66b0eb5e2e696f9263214256f3ad516a7
Durdona <durdonajurakulova98@gmail.com>
2023-09-09 20:28:07 +0500

    first commit

--- main.dart
+++ main.dart
@@ -4,35 +4,29 @@
 import 'dart:io';
 
 void main(List<String> args) 
-{ // <----------1----------->
-  // final int quest = int.parse(stdin.readLineSync() ?? '0');
+{
+  // int year = int.parse(stdin.readLineSync()!);
+  // print("Год рождения: ${birth(year)}");
+    List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-  // for(var i = 0; i <= quest; i++) {
-  //   print('Element $i');
-  // }
-  // <----------1----------->
-   
-   // <----------2----------->
-  //   final int que = int.parse(stdin.readLineSync() ?? '0');
-
-  //  for(var i = 0; i == que; i--) {
-  //   print('Element $i');
-  // }
-  // <----------2----------->
-
-  // <----------3----------->
-   
-  final int inf = int.parse(stdin.readLineSync() ?? '0');
-
-  do {
-    print('Enter the number');
-
-    
-  }  while(inf != true);
-  // <----------3----------->
-  
-
+  List<int> Sortofevennum = oddoreven(numbers: numbers);
+  print("Числа которые чёт: $Sortofevennum" );
 
   
 }
+
+// int birth(int age) {
+//   int recentYear = DateTime.now().year;
+//   int birthYear = recentYear - age;
+//   return birthYear;
+List<int> oddoreven({required List<int> numbers}) {
+  List<int> Oddnums = [];
+  numbers.forEach((number) {
+    if (number % 2 == 0) {
+      Oddnums.add(number);
+    }
+  });
+  return Oddnums;
+}
+
